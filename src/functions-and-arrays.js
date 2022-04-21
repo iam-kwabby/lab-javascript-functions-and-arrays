b4fcfdca362de93b7e7d6b7c361905664bee8d82
Kwabena Amofa <ka.amofakesewah@gmail.com>
2022-04-21 07:12:21 +0200

    Finished Iteration 4.1

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -63,16 +63,29 @@ function sum(arr) {
 // Level 1: Array of numbers
 const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+function averageNumbers(arr) {
+
+  return sumNumbers(arr) / arr.length
+
+}
 
 
 // Level 2: Array of strings
 const wordsArr = ['seat', 'correspond', 'linen', 'motif', 'hole', 'smell', 'smart', 'chaos', 'fuel', 'palace'];
 
-function averageWordLength() { }
+function averageWordLength(arr) {
+  let sum = 0;
+  for (let word of arr) {
+    sum += word.length
+  }
+  return sum / arr.length
+
+ }
 
 // Bonus - Iteration #4.1
-function avg() {}
+function avg(arr) {
+  return sum(arr) / arr.length
+}
 
 // Iteration #5: Unique arrays
 const wordsUnique = [
